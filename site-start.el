88086bf3dd7f2b90836cb5ba66feacff4885bce6
ktetzlaff <github@tetzco.de>
2026-04-16 17:44:49 +0200

    site-start.el: modernize, avoid Debian specifics whith `emacs --no-init-file ...`
    Original Debian Emacs packages skip the Debian specific startup steps when Emacs
    is started with -q/--no-init-file. We now do the same.
    
    The commit also adds comments to document what is done (and why).
    
    In addition, the file has been adapted to 'modern' elisp coding standards (adding
    lexical-binding cookie and some structural comments).

--- site-start.el
+++ site-start.el
@@ -1,14 +1,33 @@
-;;; site-start.el --- Debian utilities
+;;; site-start.el --- add Debian specific steps to Emacs startup  -*- lexical-binding: t; -*-
 
-;;; Make sure that we have instrumented all Debian packages
+;;; Commentary:
+
+;; Make sure that Debian elisp packages get initialized (except when starting
+;; Emacs with -q/--no-init-file).
+
+;;; Code:
 
 ;; Load debian-startup.el and run startup
-(unless (boundp 'debian-emacs-flavor)
-  (load "debian-startup")
-  ;; Now instrument all of the packages
-  (defconst debian-emacs-flavor 'emacs-snapshot
-    "A symbol representing the particular debian flavor of emacs that's
-running.  Something like 'emacs20, 'xemacs20, etc.")
-  (debian-startup debian-emacs-flavor))
+(defconst debian-emacs-flavor 'emacs-snapshot
+  "A symbol representing the particular debian flavor of Emacs running.
+Something like \='emacs20, \='xemacs20, \='emacs-snapshot, etc.")
 
-(add-to-list 'load-path "/usr/share/emacs/site-lisp")
+;; When emacs was started with -q/--no-init-file, `init-user-file' gets set to
+;; nil. Use this to skip loading `debian-startup' (as in the original Debian
+;; emacs packages).
+(when init-file-user
+  (if (load "debian-startup" t t nil)
+      ;; Now instrument all of the packages
+      (debian-startup debian-emacs-flavor))
+
+  ;; Adding '/usr/share/emacs/site-lisp' should not be necessary, since
+  ;; /usr/share/<emacs-snapshot>/site-lisp is (indirectly) already in
+  ;; `load-path'. However, some packages refuse to install when
+  ;; `debian-emacs-flavor' is 'emacs-snapshot' (why?), so it might be better to
+  ;; keep it.
+  ;;
+  ;; Maybe there are to this downsides I'm not aware of, but that would be a
+  ;; good reason to use a flavor different from 'emacs-snapshot'.
+  (add-to-list 'load-path "/usr/share/emacs/site-lisp"))
+
+;;; site-start.el ends here
